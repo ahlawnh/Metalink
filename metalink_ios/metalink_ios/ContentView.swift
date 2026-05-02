0478f7fbfc4e18ab254d58f20b000c0331bcc3ad
entl210 <edwardlamhitech@gmail.com>
2026-05-02 14:03:46 -0700

    Adding required xcodekeys

--- metalink_ios/metalink_ios/ContentView.swift
+++ metalink_ios/metalink_ios/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, Meta!")
+            Text("Hello, world!")
         }
         .padding()
     }
